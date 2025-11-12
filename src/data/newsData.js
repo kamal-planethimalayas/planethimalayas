fbfdd1f8852d9eec18f4f15fdef435bd7ec73fd7
Kamal Das <planethimalayas@gmail.com>
2025-11-12 15:37:53 +0530

    update site content

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -6,21 +6,31 @@ export const news = [
   head: "Weather%20Updates", 
   thumbnail: "/images/main/imd-logo1.png", // 👈 put IMD logo here
   details: `
-    <p>• Treks are underway on all slopes.</p>
+    <p>• Our treks are operational on all slopes.</p>
   `
 },
 
   {
-    title: "No active discount offer/s",
+    title: "No active discount offer at the moment. For group booking discounts or children’s scholarships, please contact us.",
     type: "news",
     head: "Offers",
     thumbnail: "/images/main/offer-logo.png",
-    details: `<p>• No active discount offer/s</p>
-  <h3>Conditions:</h3>
+    details: `
+  <h3>Offer Conditions:</h3>
 <ul>
   <li>• Discount applies <strong>only on trek fees</strong>. Add-ons (travel, offloading, rentals) are billed at original prices.</li>
   <li>• Offer is <strong>not valid</strong> on advance booking system.</li>
   <li>• Full payment must be made through our official website booking form with proper authentication.</li>
+</ul>
+<h3>Group Discount Conditions:</h3>
+<ul>
+  <li>• For group bookings of 8 participants, the trek fee for one person will be waived</li>
+  <li>• Discount applies <strong>only on trek fees</strong>. Add-ons (travel, offloading, rentals) are billed at original prices.</li>
+</ul>
+<h3>Children Scholarship:</h3>
+<ul>
+  <li>• Children under 15 years of age are eligible for a 30% scholarship on the trek fee.</li>
+  <li>• Discount applies <strong>only on trek fees</strong>. Add-ons (travel, offloading, rentals) are billed at original prices.</li>
 </ul>`  
 },
   
@@ -29,7 +39,7 @@ export const news = [
   type: "news",
   head: "Slopes",
   thumbnail: "/images/main/brand-logo-news.png",
-  details: `<p>• Our first batch of the season completed the trek successfully with holding great spirit despite of overcast conditions and hidden summit view.</p>
+  details: `<p>• Our first batch of the season to Deoriatal-Chandrashila completed the trek successfully showing great spirit despite of overcast conditions and hidden summit view.</p>
   <p>• Our last batch to Rupin Pass returned back from upper waterfall campsite due to heavy snowfall</p>`
 },
 
