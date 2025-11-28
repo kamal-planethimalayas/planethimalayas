da2fd9546a16728b075c3a45a4cf8cbc21a842fa
Kamal Das <planethimalayas@gmail.com>
2025-11-28 12:53:21 +0530

    update site content

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
+import "./AuthPage.css";
 
 export default function AuthPage() {
   const [email, setEmail] = useState("");
